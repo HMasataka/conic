92900420b7921da0ea8a01436bb93d629772df21
Masataka Hisasue <masataka.hisasue@gmail.com>
2025-02-21 17:09:54 +0900

    Feat upgrade request

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/HMasataka/conic"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/ws", conic.Handle)
+
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		log.Println(err)
+	}
 }
