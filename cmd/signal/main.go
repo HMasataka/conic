cfa18d8b2e8503134298c6f1b5818d69ef6927c5
Masataka Hisasue <masataka.hisasue@gmail.com>
2025-02-26 18:38:49 +0900

    Feat signal server

---
+++ cmd/signal/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/HMasataka/conic"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	hub := conic.NewHub()
+	socket := conic.NewSocket(hub)
+
+	r.Get("/ws", socket.Serve)
+
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		log.Println(err)
+	}
+}
